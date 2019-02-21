3d12e3846b5595f71fa43e646a3d7a4b7535d7c9
Richard Remer <rich.remer@gmail.com>
2019-02-21 00:01:40 -0800

    remove EventBox wrapper for now

--- src/gwatt.c
+++ src/gwatt.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define GWATT_REFRESH_INTERVAL 15
+// #define GWATT_EVENTS
 
 extern unsigned char gwatt_css[];
 extern unsigned int gwatt_css_len;
@@ -76,14 +77,20 @@ int main(int argc, char* argv[]) {
   gtk_level_bar_set_min_value(GTK_LEVEL_BAR(meter), 0.0);
   gtk_level_bar_set_max_value(GTK_LEVEL_BAR(meter), 1.0);
 
+#ifdef GWATT_EVENTS
   ebox = gtk_event_box_new();
   gtk_container_add(GTK_CONTAINER(ebox), meter);
+#endif
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window), "Power Meter");
   gtk_window_set_default_size(GTK_WINDOW(window), 250, 50);
   gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+#ifdef GWATT_EVENTS
   gtk_container_add(GTK_CONTAINER(window), ebox);
+#else
+  gtk_container_add(GTK_CONTAINER(window), meter);
+#endif
   g_signal_connect(G_OBJECT(window), "destroy", gtk_main_quit, NULL);
 
   g_timeout_add_seconds(GWATT_REFRESH_INTERVAL, refresh_meter, meter);
